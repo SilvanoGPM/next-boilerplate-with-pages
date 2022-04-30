fb8e36bc67d5e82a1c0ed18ffe0ac64bb9a61167
SkyG0D <silvanosilvino@hotmail.com>
2022-04-30 02:50:52 -0300

    chore: pwa config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,13 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Next Boilerplate</title>
+
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+
         <meta name="description" content="Boilerplate for NextJS projects" />
+        <meta name="theme-color" content="#FFFFFF" />
       </Head>
 
       <Component {...pageProps} />
